76543e7e48e6bf2cd288145f2c8a64a63913006b
OVOMARK <mcncstatefan@icloud.com>
2022-11-13 13:08:33 -0500

    reworked routing for db additionals

--- routes/notes.js
+++ routes/notes.js
@@ -2,9 +2,7 @@ const notes = require('express').Router();
 const path = require('path');
 const { readAndAppend, readFromFile } = require('./fsUtils');
 
-notes.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'))
-  });
+
 
 
 
