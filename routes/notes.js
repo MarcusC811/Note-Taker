21d8990843e8adadba4c15ec186ca8de180c6471
OVOMARK <mcncstatefan@icloud.com>
2022-11-13 13:37:59 -0500

    ask BCSS commit

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,15 @@ const notes = require('express').Router();
 const path = require('path');
 const { readAndAppend, readFromFile } = require('./fsUtils');
 
+notes.post('/notes', (req, res) => {
+  console.log(req.body)
 
+  // const newNote = {
+
+  // }
+
+  // readAndAppend(newTip, './db/tips.json');
+})
 
 
 
